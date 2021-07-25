a5fb000fdf8fe0a88b128a83510a7dbe8c2226bc
HoangAnhPTIT <caohoanganh2000@gmail.com>
2021-07-25 14:18:47 +0700

    complete filter

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const productRouter = express.Router()
-const { create, findAll, findOne, update } = require('../controllers/productController')
+const { create, findAll, findOne, update, findByCategory } = require('../controllers/productController')
 const { loginRequired, adminRequired } = require('../controllers/userController')
 // productRouter.post('/product/create', loginRequired, adminRequired, create)
 productRouter.post('/product/create', create)
@@ -8,5 +8,6 @@ productRouter.post('/product/create', create)
 productRouter.get('/products', findAll)
 productRouter.get('/product/:id', findOne)
 productRouter.patch('/product/:id', loginRequired, adminRequired, update)
+productRouter.get('/products/category/', findByCategory)
 
 module.exports = productRouter
